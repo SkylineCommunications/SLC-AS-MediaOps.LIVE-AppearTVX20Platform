01926db1d83f75bc84db872eee35c036e832a3a0
Joey Vanhalst <97456927+JoeyVanhalstSLC@users.noreply.github.com>
2026-03-30 12:18:12 +0200

    updates

--- AppearTV_X20_Platform_ConnectionHandler/AppearTV_X20_Platform_ConnectionHandler.cs
+++ AppearTV_X20_Platform_ConnectionHandler/AppearTV_X20_Platform_ConnectionHandler.cs
@@ -9,7 +9,9 @@ namespace AppearTVX20PlatformConnectionHandler
     using Skyline.DataMiner.ConnectorAPI.Appear.X;
     using Skyline.DataMiner.ConnectorAPI.Appear.X.InterApp.Calls;
     using Skyline.DataMiner.DataSources.Appear.X.Schema;
+    using Skyline.DataMiner.DataSources.Appear.X.Schema.IpGateway.V1_31.Input.Command;
     using Skyline.DataMiner.DataSources.Appear.X.Schema.IpGateway.V1_31.Output.Command;
+    using Skyline.DataMiner.DataSources.Appear.X.Schema.Types.Input.V1_24.Type;
     using Skyline.DataMiner.DataSources.Appear.X.Schema.Types.Output.V1_29.Type;
     using Skyline.DataMiner.Solutions.MediaOps.Live.API.Enums;
     using Skyline.DataMiner.Solutions.MediaOps.Live.Automation.Mediation.ConnectionHandlers;
@@ -54,18 +56,20 @@ namespace AppearTVX20PlatformConnectionHandler
             {
                 foreach (var updatedRow in update.UpdatedRows.Values)
                 {
-                    var key = Convert.ToString(updatedRow[1]); // destination endpoint identifier
-                    var sourceKey = Convert.ToString(updatedRow[20]); // source endpoint identifier
+                    var destinationIdentifier = Convert.ToString(updatedRow[1]); // key
+                    var inputSlot = Convert.ToInt32(updatedRow[12]); // input slot number
+                    var sourceKey = Convert.ToString(updatedRow[20]); // source key
+                    var sourceIdentifier = $"{inputSlot}.{sourceKey}";
 
-                    var endpoints = connectionEngine.Api.Endpoints.GetByElementAndIdentifiers(update.DmsElementId, new[] { key, sourceKey });
+                    var endpoints = connectionEngine.Api.Endpoints.GetByElementAndIdentifiers(update.DmsElementId, new[] { destinationIdentifier, sourceIdentifier });
                     
-                    var destinationEndpoint = endpoints.FirstOrDefault(e => e.Identifier == key);
+                    var destinationEndpoint = endpoints.FirstOrDefault(e => e.Identifier == destinationIdentifier);
                     if (destinationEndpoint == null)
                     {
                         continue;
                     }
 
-                    var sourceEndpoint = endpoints.FirstOrDefault(e => e.Identifier == sourceKey);
+                    var sourceEndpoint = endpoints.FirstOrDefault(e => e.Identifier == sourceIdentifier);
                     if (sourceEndpoint == null)
                     {
                         // unknown connection
@@ -109,79 +113,38 @@ namespace AppearTVX20PlatformConnectionHandler
             {
                 var elementId = group.Key.Value;
                 var element = engine.FindElement(elementId.AgentId, elementId.ElementId);
-
-                var appearElement = new AppearXElement(Engine.SLNetRaw, element.DmaId, element.ElementId) 
-                {
-                    Timeout = TimeSpan.FromSeconds(30),
-                };
+                var appearElement = new AppearXElement(Engine.SLNetRaw, element.DmaId, element.ElementId);
 
                 foreach (var connection in group)
                 {
                     var sourceEndpoint = connection.SourceEndpoint;
+                    if (!TryParseEndpointIdentifier(sourceEndpoint.Identifier, out var inputSlotId, out var inputKey))
+                    {
+                        engine.Log($"Source endpoint identifier '{sourceEndpoint.Identifier}' is not in correct format. It should be '[slot id].[input key]'.");
+                    }
+
+                    if (!TryGetIpGatewayInput(appearElement, inputSlotId, inputKey, out var input))
+                    {
+                        engine.Log($"Could not retrieve IpGatewayInput with key '{inputKey}' in slot '{inputSlotId}'.");
+                    }
 
                     var destinationEndpoint = connection.DestinationEndpoint;
-                    var destinationEndpointIdentifierParts = destinationEndpoint.Identifier.Split('.');
-                    if (destinationEndpointIdentifierParts.Length != 2) 
+                    if (!TryParseEndpointIdentifier(destinationEndpoint.Identifier, out var outputSlotId, out var outputKey))
                     {
                         engine.Log($"Destination endpoint identifier '{destinationEndpoint.Identifier}' is not in correct format. It should be '[slot id].[output key]'.");
-                        continue;
                     }
 
-                    if (!Int32.TryParse(destinationEndpointIdentifierParts[0], out var slotId)) 
+                    if (!TryGetIpGatewayOutput(appearElement, outputSlotId, outputKey, out var output))
                     {
-                        engine.Log($"Destination endpoint identifier '{destinationEndpoint.Identifier}' does not start with a valid slot id.");
-                        continue;
+                        engine.Log($"Could not retrieve IpGatewayOutput with key '{outputKey}' in slot '{outputSlotId}'.");
                     }
 
-                    // retrieve existing items
-                    var getItemsRequest = new GenericRequest
-                    {
-                        Slot = slotId,
-                        RequestType = GenericRequest.ReqType.IpGatewayOutputs,
-                        RequestVerb = GenericRequest.ReqVerb.Get,
-                        RequestData = GetItemsRequestSerialized(slotId),
-                    };
+                    // TODO: update service to be received by output
+                    
 
-                    var getItemsResponse = appearElement.SendMessage(getItemsRequest);
-                    if (!getItemsResponse.Success)
+                    if (!TryUpdateIpGatewayOutput(appearElement, outputSlotId, output))
                     {
-                        engine.Log($"Retrieving IpGatewayOutputs from appear element '{elementId}' failed: {getItemsResponse.ResponseMessage}.");
-                        continue;
-                    }
-
-                    if (!Guid.TryParse(destinationEndpointIdentifierParts[1], out var outputKey))
-                    {
-                        engine.Log($"Destination endpoint identifier '{destinationEndpoint.Identifier}' does not end with a valid output key.");
-                        continue;
-                    }
-
-                    // select the item to edit
-                    var apiResponse = SecureNewtonsoftDeserialization.DeserializeObject<GetOutputs.Response>(getItemsResponse.ResponseMessage)?.Result.Data;
-                    var itemToEdit = apiResponse?.Find(x => x.Key == outputKey);
-                    if (itemToEdit == null)
-                    {
-                        engine.Log($"No IpGatewayOutput with key '{outputKey}' found in appear element '{elementId}'.");
-                        continue;
-                    }
-
-                    // update service to be received by output
-
-
-                    var request = new GenericRequest
-                    {
-                        Slot = slotId,
-                        RequestType = GenericRequest.ReqType.IpGatewayOutputs, // providing the type will automatically refresh the linked tables
-                        RequestVerb = GenericRequest.ReqVerb.Post, // Post to update the item
-                        RequestData = SetItemsRequestSerialized(slotId, itemToEdit),
-                    };
-                    var response = appearElement.SendMessage(request);
-                    if (response.Success)
-                    {
-                        
-                    }
-                    else
-                    {
-                        engine.Log($"Updating IpGatewayOutput with key '{outputKey}' failed: {response.ResponseMessage}.");
+                        engine.Log($"Failed to update IpGatewayOutput with key '{outputKey}' in slot '{outputSlotId}'.");
                     }
                 }
             }
@@ -200,6 +163,92 @@ namespace AppearTVX20PlatformConnectionHandler
                    
                 }
             }
+        }
+
+        private static bool TryParseEndpointIdentifier(string identifier, out int slotId, out Guid key)
+        {
+            slotId = 0;
+            key = Guid.Empty;
+
+            var identifierParts = identifier.Split('.');
+            if (identifierParts.Length != 2)
+            {
+                return false;
+            }
+
+            if (!Int32.TryParse(identifierParts[0], out slotId))
+            {
+                return false;
+            }
+
+            if (!Guid.TryParse(identifierParts[1], out key))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool TryGetIpGatewayInput(AppearXElement element, int slotId, Guid key, out Map<Guid, Input> input)
+        {
+            input = null;
+
+            var request = new GenericRequest
+            {
+                Slot = slotId,
+                RequestType = GenericRequest.ReqType.IpGatewayInputs,
+                RequestVerb = GenericRequest.ReqVerb.Get,
+                RequestData = GetItemsRequestSerialized(slotId),
+            };
+
+            var response = element.SendMessage(request);
+            if (!response.Success)
+            {
+                return false;
+            }
+
+            var apiResponse = SecureNewtonsoftDeserialization.DeserializeObject<GetInputs.Response>(response.ResponseMessage)?.Result.Data;
+
+            input = apiResponse?.Find(x => x.Key == key);
+            return input != null;
+        }
+
+        private static bool TryGetIpGatewayOutput(AppearXElement element, int slotId, Guid key, out Map<Guid, Output> output)
+        {
+            output = null;
+
+            var request = new GenericRequest
+            {
+                Slot = slotId,
+                RequestType = GenericRequest.ReqType.IpGatewayOutputs,
+                RequestVerb = GenericRequest.ReqVerb.Get,
+                RequestData = GetItemsRequestSerialized(slotId),
+            };
+
+            var response = element.SendMessage(request);
+            if (!response.Success)
+            {
+                return false;
+            }
+
+            var apiResponse = SecureNewtonsoftDeserialization.DeserializeObject<GetOutputs.Response>(response.ResponseMessage)?.Result.Data;
+
+            output = apiResponse?.Find(x => x.Key == key);
+            return output != null;
+        }
+
+        private static bool TryUpdateIpGatewayOutput(AppearXElement element, int slotId, Map<Guid, Output> output)
+        {
+            var request = new GenericRequest
+            {
+                Slot = slotId,
+                RequestType = GenericRequest.ReqType.IpGatewayOutputs, // providing the type will automatically refresh the linked tables
+                RequestVerb = GenericRequest.ReqVerb.Post, // Post to update the item
+                RequestData = SetItemsRequestSerialized(slotId, output),
+            };
+
+            var response = element.SendMessage(request);
+            return response.Success;
         }
 
         private static string GetItemsRequestSerialized(int slot)
